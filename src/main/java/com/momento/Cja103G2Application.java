e92ee2441913c458e92e39c69bc2e687a88e3aec
Adol Christin <vivdvidjp@gmail.com>
2026-01-07 14:04:28 +0800

    First Commit

---
+++ src/main/java/com/momento/Cja103G2Application.java
@@ -0,0 +1,13 @@
+package com.momento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cja103G2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cja103G2Application.class, args);
+	}
+
+}
