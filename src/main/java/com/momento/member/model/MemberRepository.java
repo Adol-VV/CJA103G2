37b31eff1631d546d92cf257a0194cd54ba21835
L <luopei789@gmail.com>
2026-01-18 00:33:09 +0800

    會員註冊_0118

---
+++ src/main/java/com/momento/member/model/MemberRepository.java
@@ -0,0 +1,22 @@
+package com.momento.member.model;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+public interface MemberRepository extends JpaRepository<MemberVO, Integer>{
+	
+	@Transactional
+	@Modifying
+	@Query(value = "delete from member where member_id =?1", nativeQuery = true)
+	public void deleteByMemberId(int memberId);
+	
+	public MemberVO findByAccount(String account);
+	
+	public List<MemberVO> findByName(String name);
+}
---
+++ src/main/java/com/momento/member/model/MemberService.java
@@ -0,0 +1,47 @@
+package com.momento.member.model;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service("memberService")
+public class MemberService {
+	
+	@Autowired
+	MemberRepository repository;
+	
+	public void register(MemberVO memberVO){
+		repository.save(memberVO);
+	}
+	
+	public void updateMember(MemberVO memberVO) {
+		repository.save(memberVO);
+	}
+	
+	public void deleteMember(Integer memberId) {
+		if(repository.existsById(memberId)) {
+			repository.deleteByMemberId(memberId);
+		}
+	}
+	
+	public List<MemberVO> getAll(){
+		return repository.findAll();
+	}
+	
+	public MemberVO findOneMember(Integer memberId) {
+		Optional<MemberVO> optional = repository.findById(memberId);
+		return optional.orElse(null);
+	}
+	
+	public List<MemberVO> findByNameLike(String name){
+		return repository.findByName(name);
+	}
+	
+	public MemberVO findByAccount(String account) {
+		Optional<MemberVO> optional = Optional.ofNullable(repository.findByAccount(account));
+		return optional.orElse(null);
+	}
+}
