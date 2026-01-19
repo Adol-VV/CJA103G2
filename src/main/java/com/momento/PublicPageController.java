053716d6f27732c3129510428b33990361f07099
Angela <lkasjhdfg5@gmail.com>
2026-01-19 19:36:03 +0800

    從PublicPage移除主辦方

--- src/main/java/com/momento/PublicPageController.java
+++ src/main/java/com/momento/PublicPageController.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class PublicPageController {
 
-    @GetMapping("/organizer/apply")
-    public String organizerApply() {
-        return "pages/public/organizer-apply";
+    @GetMapping("/user/login")
+    public String userLogin() {
+        return "pages/user/login";
     }
 
     @GetMapping("/terms")
@@ -21,14 +21,14 @@ public class PublicPageController {
     public String privacy() {
         return "pages/public/privacy";
     }
-    
+
     @GetMapping("cart")
-	public String listaalEmp(Model model) {
-		return "pages/user/cart";
-	}
-    
+    public String listaalEmp(Model model) {
+        return "pages/user/cart";
+    }
+
     @GetMapping("checkout")
-	public String asdsadsad(Model model) {
-		return "pages/user/checkout";
-	}
+    public String asdsadsad(Model model) {
+        return "pages/user/checkout";
+    }
 }
