ec44e6aa76b5e898bfef14d0fc0614275e20ca3e
youpei <lilian830317@gmail.com>
2026-01-21 03:20:50 +0800

    主辦方通知_0121
    還在修改中

--- src/main/java/com/momento/notify/controller/OrganizerNotifyController.java
+++ src/main/java/com/momento/notify/controller/OrganizerNotifyController.java
@@ -1,7 +1,9 @@
 package com.momento.notify.controller;
 
+import com.momento.emp.model.EmpVO;
 import com.momento.notify.model.OrganizerNotifyService;
 import com.momento.notify.model.OrganizerNotifyVO;
+import com.momento.organizer.model.OrganizerVO;
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
 import jakarta.validation.constraints.Digits;
@@ -10,10 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -27,7 +26,7 @@ public class OrganizerNotifyController {
     OrganizerNotifyService orgNotifySvc;
 
     //取得主辦方的所有通知
-    @PostMapping("getOrganizerNotifications")
+    @GetMapping("getOrganizerNotifications")
     public String getOrganizerNotifications(
     // 接收請求參數, 輸入格式的錯誤處理
             @NotEmpty(message = "主辦方編號: 請勿空白")
@@ -70,17 +69,66 @@ public class OrganizerNotifyController {
                     return "pages/organizer/dashboard";
             }
 
-            // 錯誤處理
-            @ExceptionHandler(value = {ConstraintViolationException.class})
-            public ModelAndView handleError(ConstraintViolationException e){
-                Set<ConstraintViolation<?>> violations = e.getConstraintViolations();
-                StringBuilder strBuilder = new StringBuilder();
-                for (ConstraintViolation<?> violation: violations){
-                    strBuilder.append(violation.getMessage()).append("<br>");
-                }
-                String message = strBuilder.toString();
-                return new ModelAndView("pages/organizer/dashboard", "errorMessage", "請修正以下錯誤:<br>"+message);
-            }
+//    @PostMapping("/addNotify")
+//    public String addNotify(@ModelAttribute("organizerNotifyVO") OrganizerNotifyVO vo, RedirectAttributes radirect){
+//        orgNotifySvc.addNotify(vo);
+//        radirect.addFlashAttribute("message", "成功發送通知");
+//        return "redirect:/organizer/notify/send-page";
+//    }
+    @PostMapping("/addNotify")
+    public String addNotify(@ModelAttribute("organizerNotifyVO") OrganizerNotifyVO vo) {
+
+        // 1. 解決 EMP_ID 不能為 null 的問題
+        // 因為資料庫規定一定要有員工 ID，我們先暫時塞一個資料庫裡已經有的員工編號 (例如 1)
+        EmpVO tempEmp = new EmpVO();
+        tempEmp.setEmpId(1); // 請確認資料庫 EMP 表中是否有編號 1 的員工
+        vo.setEmpVO(tempEmp);
+
+        // 2. 同時也要塞入主辦方 ID (ORGANIZER_ID)
+        // 否則接下來可能會換成報 ORGANIZER_ID cannot be null
+        OrganizerVO tempOrg = new OrganizerVO();
+        tempOrg.setOrganizerId(1); // 假設目前登入的主辦方編號是 1
+        vo.setOrganizerVO(tempOrg);
+
+        // 3. 執行儲存
+        orgNotifySvc.addNotify(vo);
+
+        return "redirect:/organizer/notify/send-page";
+    }
+
+    @GetMapping("/send-page")
+    public String showSendPage(ModelMap model){
+        List<OrganizerNotifyVO> list = orgNotifySvc.getByOrgId(1); // 假設目前主辦方 ID 為 1
+        model.addAttribute("notifyListData", list);
+        model.addAttribute("organizerNotifyVO", new OrganizerNotifyVO());
+        model.addAttribute("targetPanel", "send-page");
+
+//        model.addAttribute("notifyListData", orgNotifySvc.getAll());
+        return "pages/organizer/dashboard";
+    }
+
+    @GetMapping("/getOrganizerNotify")
+    public String getOrganizerNotify (
+        @RequestParam String organizerId, ModelMap model){
+        List<OrganizerNotifyVO> list = orgNotifySvc.getByOrgId(Integer.valueOf(organizerId));
+        model.addAttribute("notifyListData", list);
+        model.addAttribute("targetPanel", "notifications"); // 讓通知中心亮起
+        return "pages/organizer/dashboard";
+    }
+
+    // 錯誤處理
+    @ExceptionHandler(value = {ConstraintViolationException.class})
+    public ModelAndView handleError(ConstraintViolationException e){
+        Set<ConstraintViolation<?>> violations = e.getConstraintViolations();
+        StringBuilder strBuilder = new StringBuilder();
+        for (ConstraintViolation<?> violation: violations){
+            strBuilder.append(violation.getMessage()).append("<br>");
+        }
+        String message = strBuilder.toString();
+        return new ModelAndView("pages/organizer/dashboard", "errorMessage", "請修正以下錯誤:<br>"+message);
+    }
+
+
 
 
 }
