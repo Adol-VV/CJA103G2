a84465afa28e0db8b766ad7f12a6caa81c3eceb8
youpei <lilian830317@gmail.com>
2026-01-28 21:16:31 +0800

    系統通知_0128-3

--- src/main/java/com/momento/member/controller/MemberCenterController.java
+++ src/main/java/com/momento/member/controller/MemberCenterController.java
@@ -6,6 +6,7 @@ import com.momento.member.model.MemberService;
 import com.momento.member.model.MemberVO;
 import com.momento.notify.model.SystemNotifyService;
 import com.momento.notify.model.SystemNotifyVO;
+import com.momento.prod.model.ProdFavService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,21 +16,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.bind.annotation.*;
 
-import com.momento.member.model.MemberService;
-import com.momento.member.model.MemberVO;
-import com.momento.prod.model.ProdFavService;
-import com.momento.event.model.EventService;
-import com.momento.event.dto.EventListItemDTO;
-
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
