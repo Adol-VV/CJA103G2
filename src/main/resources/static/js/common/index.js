87c8dcc1c1bc20ddd49f300c6b88221aa85e80d3
Angela <lkasjhdfg5@gmail.com>
2026-01-29 23:25:50 +0800

    後台權限管理_0129
    員工管理/權限管理拆開控管
    員工管理:編輯員工資料
    員工管理:管理者加上員工密碼重設置功能
    員工UX提升:快捷變更密碼
    權限管理:管理畫面雛形

--- src/main/resources/static/js/common/index.js
+++ src/main/resources/static/js/common/index.js
@@ -2,18 +2,18 @@
  * 前端共用 UI 模組的「總入口初始化檔」
  */
 import AuthUI from './auth.js';
-import CartUI from './cart.js';
+// import CartUI from './cart.js';  // 後台不需要購物車功能，且 cart.js 使用舊式寫法
 import NavbarUI from './navigation.js';
 import ToastUI from './toast.js';
 
 document.addEventListener('DOMContentLoaded', () => {
     AuthUI.updateState();
-    CartUI.updateBadge();
+    // CartUI.updateBadge();  // 後台不需要購物車
     NavbarUI.highlightCurrent();
 });
 
 // Export for usage if needed
-export { AuthUI, CartUI, NavbarUI, ToastUI };
+export { AuthUI, NavbarUI, ToastUI };
 
 // Expose globals for legacy scripts
 window.showToast = (msg, type) => ToastUI.show(msg, type);
