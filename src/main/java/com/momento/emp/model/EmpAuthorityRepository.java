e02fb5a0c69d022205bf5490e25424483bb3aa8a
Angela <lkasjhdfg5@gmail.com>
2026-01-30 19:22:25 +0800

    後台權限管理_0130
    權限管理操作邏輯完善

--- src/main/java/com/momento/emp/model/EmpAuthorityRepository.java
+++ src/main/java/com/momento/emp/model/EmpAuthorityRepository.java
@@ -1,6 +1,8 @@
 package com.momento.emp.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -8,16 +10,18 @@ import java.util.List;
 @Repository
 public interface EmpAuthorityRepository extends JpaRepository<EmpAuthorityVO, Integer> {
 
-    boolean existsByEmpIdAndFunctionId(Integer empId, Integer functionId);
+    // 透過關聯物件的 ID 進行查詢
+    List<EmpAuthorityVO> findByEmp_EmpId(Integer empId);
 
-    List<EmpAuthorityVO> findByEmpId(Integer empId);
+    List<EmpAuthorityVO> findByFunction_FunctionId(Integer functionId);
 
-    List<EmpAuthorityVO> findByFunctionId(Integer functionId);
+    // 為了相容性與 Clean Code，提供基於 ID 的快速檢查
+    boolean existsByEmp_EmpIdAndFunction_FunctionId(Integer empId, Integer functionId);
 
-    void deleteByEmpIdAndFunctionId(Integer empId, Integer functionId);
+    @Modifying
+    void deleteByEmp_EmpIdAndFunction_FunctionId(Integer empId, Integer functionId);
 
-    // 用於更新權限時，先清空該員工所有舊權限
-    @org.springframework.data.jpa.repository.Modifying
-    @org.springframework.data.jpa.repository.Query("DELETE FROM EmpAuthorityVO ea WHERE ea.empId = ?1")
+    @Modifying
+    @Query("DELETE FROM EmpAuthorityVO ea WHERE ea.emp.empId = ?1")
     void deleteByEmpId(Integer empId);
 }
